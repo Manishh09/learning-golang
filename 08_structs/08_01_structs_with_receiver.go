07442c74a661848f737e0c47433e140b167a2831
Manish Boge (SX/EDC2) <manish.boge@in.bosch.com>
2024-09-06 16:27:01 +0530

    struct with receiver and pass-by-value

---
+++ 08_structs/08_01_structs_with_receiver.go
@@ -0,0 +1,91 @@
+package main
+
+import "fmt"
+
+type employee struct {
+	name        string
+	designation string
+	address     address
+}
+
+type address struct {
+	zipCode  int
+	location string
+}
+
+func main() {
+	// first approach
+	// emp := employee{
+	// 	"S",
+	// 	"B",
+	// }
+
+	// using var
+	// var emp2 employee
+	// fmt.Println(emp2)
+
+	// emp2.designation = "Go dev"
+	// emp2.name = "Manish"
+
+	emp3 := employee{
+		name:        "A",
+		designation: "Go dev",
+		address: address{
+			zipCode:  500089,
+			location: "Hyd",
+		},
+	}
+
+	emp3.save("Will")
+
+	emp3.showDetails()
+
+}
+
+func (e employee) showDetails() {
+	fmt.Printf("%+v", e)
+}
+
+func (e employee) save(des string) {
+	e.name = des
+}
+
+/******* Before Save
+
+	block0
+
+	block1 --- employee  , name : "A"
+
+
+	block2 ---
+
+	block3 ---
+
+
+******/
+
+/******* At Save (emp3.save("Will")) - copy of original struct value will be copied into memory
+	block0
+
+	block1 --- employee  , name : "A"
+
+
+	block2 --- employee, name : "A"
+
+	block3 ---
+
+
+******/
+
+/******* After Save - copied value will get updated but not the original
+	block0
+
+	block1 --- employee  , name : "A"
+
+
+	block2 --- employee, name : "Will"
+
+	block3 ---
+
+
+******/
